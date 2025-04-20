9cf2cee4b075f45036dfac07b2f196a0eab23816
Mike Spacek <mike@spacedigital.com>
2025-04-19 20:41:38 -0500

    Remove Admin tab and related components

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -35,12 +35,7 @@ const Footer = () => {
                   About
                 </Link>
               </li>
-              <li>
-                <Link href="/admin" className="text-gray-400 hover:text-white transition-colors flex items-center">
-                  <svg className="w-3 h-3 mr-2 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
-                  Admin
-                </Link>
-              </li>
+
             </ul>
           </div>
 
