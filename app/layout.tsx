e8b1c46bee2b9a3eb7add81bc3f5ea2c2172a6bb
Mike Spacek <mike@spacedigital.com>
2025-04-22 21:02:57 -0500

    Fix themeColor metadata warning by moving it to viewport export

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./test-globals.css";
 
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
     apple: "/images/union-favicon.png",
   },
   manifest: "/manifest.json",
+};
+
+export const viewport: Viewport = {
   themeColor: "#181818",
 };
 
